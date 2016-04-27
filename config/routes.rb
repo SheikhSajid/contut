7d5cace6e0d30562ab355376ace5dd45be3282f5
sheikhsajid <sheikh.sajid522@gmail.com>
2016-04-27 07:00:33 +0000

    Added styling and partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  # get 'pages/home'
+
+  get 'pages/about'
+
   resources :tutors
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'tutors#index'
+  root 'pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
