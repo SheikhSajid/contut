ba7c4afa0e94809e56c3c7c54029b91c5221b6e9
sheikhsajid <sheikh.sajid522@gmail.com>
2016-04-27 08:49:34 +0000

    Created students.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'students/index'
+
+  get 'students/show'
+
+  get 'students/new'
+
+  get 'students/edit'
+
+  get 'students/create'
+
+  get 'students/update'
+
+  get 'students/destroy'
+
   # get 'pages/home'
 
   get 'pages/about'
---
+++ db/migrate/20160427084611_create_students.rb
@@ -0,0 +1,16 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.integer :phone
+      t.string :city
+      t.mirpur :area
+      t.integer :zip
+      t.text :full_address
+
+      t.timestamps null: false
+    end
+  end
+end
