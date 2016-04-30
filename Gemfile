91cc98a3a49fe79b54024d8bdef8e7df2e139c3b
sheikhsajid <sheikh.sajid522@gmail.com>
2016-04-30 13:07:07 +0000

    Added some styling to profiles

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bcrypt', '~> 3.1.7'
 
 # Upload user profile pictures
-# gem 'paperclip', '~> 4.2.0'
+gem 'paperclip', '~> 4.2.0'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  helper_method :current_user, :logged_in?, :edit_profile
+  helper_method :current_user, :logged_in?, :edit_profile, :go_to_profile
   
   def current_user
     if session[:tutor_id]
@@ -29,6 +29,14 @@ class ApplicationController < ActionController::Base
       return edit_tutor_path(current_user)
     elsif session[:student_id]
       return edit_student_path(current_user)
+    end
+  end
+  
+  def go_to_profile
+    if session[:tutor_id]
+      return tutor_path(current_user)
+    elsif session[:student_id]
+      return student_path(current_user)
     end
   end
 end
